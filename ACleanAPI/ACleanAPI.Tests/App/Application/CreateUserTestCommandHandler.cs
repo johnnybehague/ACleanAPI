b3d867a057de1baa1d301ed75eafe6a16f903f64
Johnny Behague <johnny.behague@live.fr>
2025-12-26 11:53:14 +0100

    test: ajout test sur le handler

---
+++ ACleanAPI/ACleanAPI.Tests/App/Application/CreateUserTestCommandHandler.cs
@@ -0,0 +1,18 @@
+using ACleanAPI.Application.CommandHandlers;
+using ACleanAPI.Application.Interfaces;
+using ACleanAPI.Infrastructure.Interfaces;
+using ACleanAPI.Tests.Common;
+using FluentResults;
+
+namespace ACleanAPI.Tests.App.Application;
+
+public class CreateUserTestCommandHandler : AcCreateEntityCommandHandlerBase<UserTestDto, UserTestEntity>
+{
+    public CreateUserTestCommandHandler(IAcEntityRepository<UserTestEntity> repository, IAcEntityMapper<UserTestEntity, UserTestDto> mapper)
+    : base(repository, mapper)
+    {
+    }
+
+    public async Task<Result> Handle(IAcCreateEntityRequest<UserTestDto> request, CancellationToken cancellationToken)
+        => await HandleRequest(request, cancellationToken);
+}
