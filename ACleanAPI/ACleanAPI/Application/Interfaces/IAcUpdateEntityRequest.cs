2a4ccff441712932bbb3eb7642dd64cee988658b
Johnny Behague <johnny.behague@live.fr>
2026-01-09 11:35:20 +0100

    fix record bindrequired

--- ACleanAPI/ACleanAPI/Application/Interfaces/IAcUpdateEntityRequest.cs
+++ ACleanAPI/ACleanAPI/Application/Interfaces/IAcUpdateEntityRequest.cs
@@ -1,8 +1,15 @@
 using ACleanAPI.Application.Core;
 using FluentResults;
 using MediatR;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ACleanAPI.Application.Interfaces;
 
-public record AcUpdateEntityRequest<TDto>(int Id, TDto? Dto): IRequest<Result>
-    where TDto : AcEntityDtoBase;
+public record AcUpdateEntityRequest<TDto>(int id, TDto? dto): IRequest<Result>
+    where TDto : AcEntityDtoBase
+{
+    [BindRequired]
+    public int Id { get; init; } = id;
+
+    public TDto? Dto { get; init; } = dto;
+}
